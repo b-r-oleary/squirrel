2c3b29126799eb721d1b350df1e3c621a3a45162
Brendon Oleary <brendon@seatgeek.com>
2019-10-11 16:43:56 -0400

    make it a command line tool using structopt and add a readme

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@ use std::fs;
 
 use sqlparser::dialect::PostgreSqlDialect;
 use sqlparser::parser::Parser;
+use structopt::StructOpt;
 
 use squirrel::{Options, Format};
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "squirrel", about = "A ~~squirrel~~ sql formatter.")]
+struct Opt {
+    #[structopt(name = "FILE")]
+    file_name: String,
+}
+
 fn main() {
+    let opt = Opt::from_args();
+
     let dialect = PostgreSqlDialect {};
-    let sql = fs::read_to_string("./sql/example_1.sql").expect("Unable to read file");
+    let sql = fs::read_to_string(&opt.file_name).expect("Unable to read file");
 
     let ast = Parser::parse_sql(&dialect, sql).unwrap();
 
